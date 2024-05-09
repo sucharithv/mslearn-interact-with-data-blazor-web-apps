44bc40965acb346aaf245f2e06fe5f5308c126f9
Sucharith Vanguri <sucharithv@gmail.com>
2024-05-09 12:33:19 -0400

    En do Lab 7 - Added Size customization by passing data to Component

--- Data/PizzaService.cs
+++ Data/PizzaService.cs
@@ -1,14 +1,14 @@
-namespace BlazingPizza.Data;
+// namespace BlazingPizza.Data;
 
-public class PizzaService
-{
-	public async Task<Pizza[]> GetPizzasAsync()
-	{
-		// Call your data access technology here
+// public class PizzaService
+// {
+// 	public async Task<Pizza[]> GetPizzasAsync()
+// 	{
+// 		// Call your data access technology here
 
-		var pizzas = new List<Pizza>() {
-		new Pizza() { PizzaId=1, Name="Veggie", Description="Nice healthy", Price=10, Vegan=false, Vegetarian=true}
-		};
-		 return await Task.FromResult( pizzas.ToArray());
-	}
-}
+// 		var pizzas = new List<Pizza>() {
+// 		new Pizza() { PizzaId=1, Name="Veggie", Description="Nice healthy", Price=10, Vegan=false, Vegetarian=true}
+// 		};
+// 		 return await Task.FromResult( pizzas.ToArray());
+// 	}
+// }
